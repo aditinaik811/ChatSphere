da184c26c33523a57f76c072c46c2ed18de95e0d
Aditi Naik <aditinaik811@gmail.com>
2025-10-01 14:07:38 +0530

    signup code refactored with more checks

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config()
 export const connectDB = async()=>{
+    const {MONGO_URI} = process.env;
+    if(!MONGO_URI) throw new Error("MONGO_URI is not set");
     try{
-        const conn = await  mongoose.connect(process.env.MONGO_URI)
+        const conn = await  mongoose.connect(MONGO_URI)
         console.log("SUCCESSFULLY CONNECTED MONGODB:",conn.connection.host)
     }
     catch(error){
